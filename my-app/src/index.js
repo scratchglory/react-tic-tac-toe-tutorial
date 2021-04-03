86b6f56966ace4c4ec724d52bb2795a0a68e9823
scratchglory <scratchglory@gmail.com>
2021-04-03 16:14:52 -0400

    have rendering the winner and who plays next

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -64,8 +64,16 @@ import './index.css';
   
     render() {
       // change the display so it's either X or O
-      const status = `Next Player: ${this.state.xIsNext ? 'X' : 'O'}`;
-  
+      // const status = `Next Player: ${this.state.xIsNext ? 'X' : 'O'}`;
+      // call the helper function to dictate the winner
+      const winner = calculateWinner(this.state.squares)
+      let status;
+      if (winner){
+        status = `Winner: ${winner}`
+      } else {
+        status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
+      }
+
       return (
         <div>
           <div className="status">{status}</div>
@@ -105,6 +113,27 @@ import './index.css';
     }
   }
   
+  // helper function
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   // ========================================
   
   ReactDOM.render(
